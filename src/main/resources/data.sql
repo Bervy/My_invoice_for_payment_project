e371e4c32e8401ffb3c0742f372b48d88a5819c6
Vladislav <vladshelkovo@yandex.ru>
2022-09-16 12:00:14 +0300

    Create database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO general_information (general_information_id,
+                                 monthly_total,
+                                 total_penalties,
+                                 recalculations,
+                                 debt,
+                                 privileges,
+                                 paid) VALUES
+(1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
